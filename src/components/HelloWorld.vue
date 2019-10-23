a2153f5ffabca745819b99abf705679dcf96f6f6
your name <your email>
2019-10-23 22:42:54 +0800

    m 解决rem适配问题

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="hello">
+    <div class='test'></div>
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
@@ -43,6 +44,11 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
+.test{
+  width: 100px;
+  height: 100px;
+  background-color: red;
+}
 h3 {
   margin: 40px 0 0;
 }
